4fdcdc49d47f9e9c43931bc32f2c1e0a4463d2fb
+ <+>
2022-07-20 11:04:28 -0600

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+
+    function handleBlur(){
+        console.log('Hey! Eyes on me!');
+    }
+
+    function handleFocus(){
+        console.log('Good!');
+    }
+
+    return ( 
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+)
+}
+
+export default EyesOnMe;
